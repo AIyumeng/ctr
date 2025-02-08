e8e1df4bd3d2e38b61930ad3c09f85825c0ab8e3
ding yumeng <94977446+AIyumeng@users.noreply.github.com>
2025-02-08 20:08:06 +0800

    add data for criteo

--- my_model_zoo/Finalmlp/run.sh
+++ my_model_zoo/Finalmlp/run.sh
@@ -1,1 +1,3 @@
-python 'my_model_zoo/Finalmlp/run_finalmlp.py' --config 'my_model_zoo/Finalmlp/config' --expid FinalMLP_movielenslatest_x1_004_498f3e4f --gpu 0
+# python 'my_model_zoo/Finalmlp/run_finalmlp.py' --config 'my_model_zoo/Finalmlp/config' --expid FinalMLP_movielenslatest_x1_004_498f3e4f --gpu 0
+
+python 'my_model_zoo/Finalmlp/run_finalmlp.py' --config 'my_model_zoo/Finalmlp/config' --expid FinalMLP_criteo_x1_004_d5d36917  --gpu 0
